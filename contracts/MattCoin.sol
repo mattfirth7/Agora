6a8f4be4fd9c8997b13aaedfc56da997c9067716
mattfirth7 <mmf001x@yahoo.com>
2021-03-30 16:18:17 -0400

    Added transfer functionality

--- contracts/MattCoin.sol
+++ contracts/MattCoin.sol
@@ -4,9 +4,15 @@ contract MattCoin {
 	// State variables
 	string public name = "MattCoin";
 	string public symbol = "MCN";
+	string public standard = "MattCoin v1.0";
 	uint256 public totalSupply;
 	mapping(address => uint256) public balanceOf;
 
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+	);
 
 	constructor(uint256 _initialSupply) public {
 		totalSupply = _initialSupply;
@@ -14,4 +20,18 @@ contract MattCoin {
 		// allocate initial supply
 		balanceOf[msg.sender] = _initialSupply;
 	}
+
+	// Transfer
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		// Exception if account doesn't have enough
+		require(balanceOf[msg.sender] >= _value);
+		// Transfer the balance
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+		// Transfer Event
+		emit Transfer(msg.sender, _to, _value);
+		// Return a boolean
+		return true;
+	}
+
 }
